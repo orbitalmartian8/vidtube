b39d8ad337c43911c0709b0265f8e40a44eec333
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-06-16 21:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,72 @@ document.addEventListener("DOMContentLoaded", function() {
   // Fetch video data from JSON file
   fetch("videos.json")
     .then(response => response.json())
-    .then(data => displayVideos(data.videos))
+    .then(data => {
+      const videoId = getVideoIdFromUrl();
+      if (videoId) {
+        const video = data.videos.find(v => v.id === videoId);
+        if (video) {
+          displayVideo(video);
+        } else {
+          displayVideos(data.videos);
+        }
+      } else {
+        displayVideos(data.videos);
+      }
+    })
     .catch(error => console.error(error));
 });
+
+function getVideoIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
+}
+
+function displayVideo(video) {
+  const videoContainer = document.createElement("div");
+  videoContainer.className = "video-container";
+
+  const videoPlayer = document.createElement("video");
+  videoPlayer.className = "video-player";
+  videoPlayer.src = video.video_file;
+  videoPlayer.controls = true;
+  videoContainer.appendChild(videoPlayer);
+
+  const videoTitle = document.createElement("h2");
+  videoTitle.className = "video-title";
+  videoTitle.textContent = video.title;
+  videoContainer.appendChild(videoTitle);
+
+  const videoDate = document.createElement("p");
+  videoDate.className = "video-date";
+  videoDate.textContent = "Uploaded on " + formatDateTime(video.upload_date);
+  videoContainer.appendChild(videoDate);
+
+  const videoDescription = document.createElement("p");
+  videoDescription.className = "video-description";
+  videoDescription.textContent = video.description;
+  videoContainer.appendChild(videoDescription);
+
+  const videoList = document.getElementById("videoList");
+  videoList.innerHTML = "";
+  videoList.appendChild(videoContainer);
+}
 
 function displayVideos(videos) {
   const videoList = document.getElementById("videoList");
 
-  // Loop through each video and create HTML elements
   videos.forEach(video => {
     const videoContainer = document.createElement("div");
     videoContainer.className = "video-container";
 
+    const videoLink = document.createElement("a");
+    videoLink.href = "index.html?id=" + video.id;
+    videoContainer.appendChild(videoLink);
+
     const videoThumbnail = document.createElement("img");
     videoThumbnail.className = "video-thumbnail";
     videoThumbnail.src = video.thumbnail_file;
-    videoContainer.appendChild(videoThumbnail);
+    videoLink.appendChild(videoThumbnail);
 
     const videoTitle = document.createElement("h2");
     videoTitle.className = "video-title";
@@ -33,11 +83,6 @@ function displayVideos(videos) {
     videoDescription.className = "video-description";
     videoDescription.textContent = video.description;
     videoContainer.appendChild(videoDescription);
-
-    videoContainer.addEventListener("click", function() {
-      // Handle video click event, e.g., open video in custom video player
-      console.log("Video clicked:", video);
-    });
 
     videoList.appendChild(videoContainer);
   });
