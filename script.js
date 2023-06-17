a4e60048c3929c46f3ca8ad050823a34958209e5
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-06-17 10:02:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const toggleSwitch = document.getElementById("toggle");
-  toggleSwitch.addEventListener("change", toggleTheme);
-
-  fetchVideoData();
+  const videoId = getVideoIdFromUrl();
+  if (videoId) {
+    fetchVideoData(videoId);
+  }
 });
 
-function toggleTheme(event) {
-  const body = document.body;
-  if (event.target.checked) {
-    body.classList.add("dark-mode");
-  } else {
-    body.classList.remove("dark-mode");
-  }
+function getVideoIdFromUrl() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
 }
 
-function fetchVideoData() {
+function fetchVideoData(videoId) {
   fetch("videos.json")
     .then(response => response.json())
     .then(data => {
-      displayVideoList(data.videos);
+      const video = data.videos.find(v => v.id === videoId);
+      if (video) {
+        displayVideo(video);
+      } else {
+        console.error("Video not found");
+      }
     })
     .catch(error => console.error(error));
 }
 
-function displayVideoList(videos) {
-  const videoListContainer = document.getElementById("videoList");
+function displayVideo(video) {
+  const videoPlayer = document.getElementById("videoPlayer");
+  const videoTitle = document.getElementById("videoTitle");
+  const videoUploader = document.getElementById("videoUploader");
+  const videoUploadDate = document.getElementById("videoUploadDate");
+  const videoDescription = document.getElementById("videoDescription");
 
-  videos.forEach(video => {
-    const videoItem = document.createElement("div");
-    videoItem.className = "video-item";
-    videoItem.innerHTML = `
-      <a href="video.html?id=${video.id}">
-        <img class="thumbnail" src="${video.thumbnail_file}" alt="Video Thumbnail">
-        <div class="video-info">
-          <h3 class="video-title">${video.title}</h3>
-          <p class="video-uploader">Uploaded by: ${video.uploader}</p>
-          <p class="video-upload-date">Uploaded on: ${formatDateTime(video.upload_date)}</p>
-        </div>
-      </a>
-    `;
-    videoListContainer.appendChild(videoItem);
-  });
+  videoPlayer.src = video.video_file;
+  videoTitle.textContent = video.title;
+  videoUploader.textContent = "Uploaded by: " + video.uploader;
+  videoUploadDate.textContent = "Uploaded on: " + formatDateTime(video.upload_date);
+  videoDescription.textContent = video.description;
 }
 
 function formatDateTime(dateTime) {
