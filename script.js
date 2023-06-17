6b3f76c8325235785e1f608731017a43c22c3de6
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-06-17 12:35:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,8 +30,17 @@ function displayVideo(video) {
     autoplay: false,
     preload: "auto",
   });
-  videoPlayer.src({ src: video.video_file, type: video.mime_type });
-  videoPlayer.poster(video.thumbnail);
+
+  // Clear any previously loaded sources
+  videoPlayer.src([]);
+
+  // Add the video source dynamically
+  videoPlayer.src([
+    {
+      src: video.video_file,
+      type: video.mime_type,
+    },
+  ]);
 
   const videoTitle = document.getElementById("videoTitle");
   const videoUploader = document.getElementById("videoUploader");
@@ -43,6 +52,7 @@ function displayVideo(video) {
   videoUploadDate.textContent = "Uploaded on: " + formatDateTime(video.upload_date);
   videoDescription.textContent = video.description;
 }
+
 
 function formatDateTime(dateTime) {
   const options = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" };
